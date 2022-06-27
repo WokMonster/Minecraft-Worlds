9602d70a0d1f5cb6415aecbe8991b9883d813393
WokMonster <yisraelahinadab@icloud.com>
2022-06-27 13:51:11 -0500

    New Save

---
+++ Save.bat
@@ -0,0 +1,5 @@
+cd C:\Users\yisra\AppData\Local\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang\minecraftWorlds
+git add .
+git commit -m "New Save"
+git push origin main
+PAUSE
